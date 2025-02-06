9b653ec8cd4590bdf36c4111426a0063d3cc82d8
178oi <178megane@gmail.com>
2025-02-07 00:43:15 +0900

    ディブの中で色変えIF

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,41 +2,15 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [number, setNumber] = useState(0);
-  const [number2, setNumber2] = useState(0);
-  const handleIncrement = () => {
-    setNumber(number + 1);
-  };
-  const handleDeement = () => {
-    setNumber(number - 1);
-  };
-  const handleIncrement2 = () => {
-    setNumber2(number2 + 1);
-  };
-  const handleDeement2 = () => {
-    setNumber2(number2 - 1);
-  };
+  const [block1, setBlock1] = useState(0);
+  const [block2, setBlock2] = useState(0);
+  const [block3, setBlock3] = useState(0);
   return (
     <div className={styles.container}>
-      <button onClick={handleDeement} className={styles.button}>
-        -
-      </button>
-
-      <div className={styles.number}>{number}</div>
-
-      <button onClick={handleIncrement} className={styles.button}>
-        +
-      </button>
-      <button onClick={handleDeement2} className={styles.button}>
-        -
-      </button>
-
-      <div className={styles.number}>{number2}</div>
-
-      <button onClick={handleIncrement2} className={styles.button}>
-        +
-      </button>
-      <div className={styles.number}>{number + number2}</div>
+      ;
+      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
+      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
+      <div style={{ backgroundColor: block1 === 0 ? 'red' : 'blue' }} className={styles.block} />
     </div>
   );
 };
